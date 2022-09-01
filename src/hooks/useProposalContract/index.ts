6387cf3d89036a901f19fa2fe8e755d9376ac799
Julian Murphy <njmdistrisoft@gmail.com>
2022-09-01 12:22:55 -0300

    add address

--- src/hooks/useProposalContract/index.ts
+++ src/hooks/useProposalContract/index.ts
@@ -1,11 +1,18 @@
 import { useMemo } from 'react'
 import {useWeb3React } from '@web3-react/core'
+import { ProposalContractABI } from '../../config/artifacts'
+
+
+const { address, abi } = ProposalContractABI
 
 const useProposalContract = () => {
   const {active, library, chainId } = useWeb3React()
 
   const contract = useMemo(
-    () => new library.eeth.Contract("address", abi),
-    [ active, chainId, library?.eth?.Contract]
+    () => {
+      if (active)
+        return new library.eeth.Contract(abi, address[chainId])
+    },
+    [ active, chainId, library?.eth?.Contract ]
   )
 }
