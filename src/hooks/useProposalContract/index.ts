84e74611b540622cec9c951d6b78e1c3afb729c7
Julian Murphy <njmdistrisoft@gmail.com>
2022-09-01 12:17:46 -0300

    add abi contract

--- src/hooks/useProposalContract/index.ts
+++ src/hooks/useProposalContract/index.ts
@@ -4,5 +4,8 @@ import {useWeb3React } from '@web3-react/core'
 const useProposalContract = () => {
   const {active, library, chainId } = useWeb3React()
 
-  const contract = useMemo(() => new library.eeth.Contract("address", abi)),
+  const contract = useMemo(
+    () => new library.eeth.Contract("address", abi),
+    [ active, chainId, library?.eth?.Contract]
+  )
 }
