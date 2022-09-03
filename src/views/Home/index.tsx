c675d889c1ebe79982bcb32352b3369aee6f899f
Julian Murphy <njmdistrisoft@gmail.com>
2022-09-03 12:53:53 -0300

    verify selected library

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,19 +1,19 @@
-import { FC, useState, useCallback, useEffect, useContext } from 'react'
+import { useState, useCallback, useEffect, useContext } from 'react'
 import { LibraryContext } from "../../App"
 
 
-export const Home: FC = () => {
+export const Home = () => {
   const [voteNo, setVoteNo] = useState(0)
   const [voteYes, setVoteYes] = useState(0)
   const {selectedLibrary} = useContext(LibraryContext)
 
   const getVotes = useCallback(async () => {
-    if(selectedLibrary.contract) {
-    }
+    if(selectedLibrary?.contract) {
       const votes = await selectedLibrary.getVotes()
       console.log('Votos response', votes)
       setVoteNo(votes.votesForNo)
       setVoteYes(votes.votesForYes)
+    }
 
   }, [selectedLibrary])
 
