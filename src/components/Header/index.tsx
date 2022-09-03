7c1201ad7caaa84491c3569f461e5f5ea2a110ab
Julian Murphy <njmdistrisoft@gmail.com>
2022-09-03 17:00:44 -0300

    title bold

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,7 @@ export const Header = () => {
     <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-700">
       <div className="container flex flex-wrap justify-center flex-col md:flex-row md:justify-between items-center mx-auto">
         <a href="https://flowbite.com/" className="flex items-center">
-            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Proposal - PlatziChallenge</span>
+            <span className="self-center text-2xl font-bold whitespace-nowrap dark:text-white">Platzi - Challenge</span>
         </a>
         <div className="flex items-center">
           <Wallet />
