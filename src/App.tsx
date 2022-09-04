5f8848a0da01d6bf97728adf7e018a696224cf59
Julian Murphy <njmdistrisoft@gmail.com>
2022-09-04 00:59:02 -0300

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Providers } from './config/web3'
 import { Home } from './views/Home'
 import { ProposalContractABI, PROPOSAL_CONTRACT_ADDRESS_GOERLI } from './config/artifacts'
 import { Header } from './components/Header'
-import { FaGithub } from 'react-icons/fa'
+import { Footer } from './components/Footer'
 
 
 type Library = {
@@ -69,12 +69,7 @@ function App() {
           <Home switchLibrary={switchLibrary} setSwitchLibrary={setSwitchLibrary} />
         </div>
       </LibraryContext.Provider>
-      <a href="https://github.com/fkmurphy" className="flex justify-center text-sm text-gray-800 dark:text-gray-400 group">{`Created by   `}
-        <span className="ml-1 text-amber-300 group-hover:text-amber-500">
-          <FaGithub className="inline mr-1" />
-          {`Julian Murphy`}
-        </span>
-      </a>
+      <Footer />
     </div>
   );
 }
