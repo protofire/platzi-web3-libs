1e3a47d27420ac0bb4afff20f1e3f6b437835cce
IvySaskia <ivysaskia@gmail.com>
2022-09-22 12:32:49 -0400

    ADD: Web3ReactProvider

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from "@chakra-ui/react";
+import { Web3ReactProvider } from "@web3-react/core";
+import { getLibrary } from "./config/web3";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
 root.render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <Web3ReactProvider getLibrary={getLibrary}>
+        <App />
+      </Web3ReactProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
