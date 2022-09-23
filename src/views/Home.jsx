174ca794c192d28515b2826f7274b1953f670479
IvySaskia <ivysaskia@gmail.com>
2022-09-23 19:06:46 -0400

    ADD: Comented icons for deploy on vercel

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -22,7 +22,7 @@ import { useWeb3React } from "@web3-react/core";
 import { CheckCircleIcon, CloseIcon } from "@chakra-ui/icons";
 import { AppContext } from "../context/AppContext";
 import useProposalStatus from "../hooks/useProposalStatus";
-import { AiFillGithub } from "react-icons/ai";
+// import { AiFillGithub } from "react-icons/ai";
 
 const Home = () => {
   const { active } = useWeb3React();
@@ -85,7 +85,7 @@ const Home = () => {
               isExternal
             >
               <Button
-                leftIcon={<AiFillGithub />}
+                // leftIcon={<AiFillGithub />}
                 size={"lg"}
                 fontWeight={"normal"}
                 px={6}
